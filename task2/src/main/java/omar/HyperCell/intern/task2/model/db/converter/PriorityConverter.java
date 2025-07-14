d2772cba0cf5138b26c97f99ab874e8a08c2d9f5
Omar-Saad-ELGharbawy <84602951+Omar-Saad-ELGharbawy@users.noreply.github.com>
2025-07-14 05:36:15 +0300

    Modify Enums for converter and add Converter Classes

--- task2/src/main/java/omar/HyperCell/intern/task2/model/db/converter/PriorityConverter.java
+++ task2/src/main/java/omar/HyperCell/intern/task2/model/db/converter/PriorityConverter.java
@@ -1,4 +1,17 @@
 package omar.HyperCell.intern.task2.model.db.converter;
 
-public class PriorityConverter {
+import jakarta.persistence.AttributeConverter;
+import omar.HyperCell.intern.task2.model.db.enums.Priority;
+
+public class PriorityConverter implements AttributeConverter<Priority, Integer> {
+
+    @Override
+    public Integer convertToDatabaseColumn(Priority attribute) {
+        return attribute.getId();
+    }
+
+    @Override
+    public Priority convertToEntityAttribute(Integer dbData) {
+        return Priority.valueOf(dbData);
+    }
 }
